692d5d50f7142739d587d4fd2d81a9ed1d48c7b7
Aditi Killedar <73051765+aditikilledar@users.noreply.github.com>
2022-04-18 11:10:22 +0530

    Update grammar.py

---
+++ grammar.py
@@ -0,0 +1,47 @@
+""" MODULE TO PARSE PCFG GRAMMAR """
+
+from collections import defaultdict
+
+class pcfg(object):
+	"""pcfg representation"""
+
+	def __init__(self, grammarfile):
+		self.readrules(grammarfile)
+		self.startsym = None
+		self.lhsrules = defaultdict(list)
+		self.rhsrules = defaultdict(list)
+
+	def readrules(self, gramfile):
+
+		for line in gramfile:	
+			line = line.strip()
+
+			# if not commented and line exists,
+			if not line.startswith('#') and line:
+			
+				if "->" in line:
+					# if line is a rule 
+					rule = self.parse(line)
+					lhs, rhs, prob = rule
+					# add rule to dict for rhs and lhs
+					self.lhsrules[lhs].append(rule)
+					self.rhsrules[rhs].append(rule)
+
+				else: 
+					# line is a start symbol
+					start, prob = line.rsplit(';')
+					self.startsym = start.strip()
+
+	def parse(self, rule):
+
+		lhs, rest = rule.split('->')
+		lhs = lhs.strip()
+
+		rhs, prob = rest.rsplit(";")
+		rhstup = tuple(rhs.strip().split())
+		prob = float(prob)
+
+		print('lhs, rhs, prob \n', lhs, rhstup, prob)
+
+
+if __name__ == '__main__':
