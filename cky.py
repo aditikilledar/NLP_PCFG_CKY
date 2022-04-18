60a15c8b3c1e49ee7efd481cc902a6ff412901be
Aditi Killedar <73051765+aditikilledar@users.noreply.github.com>
2022-04-18 12:41:21 +0530

    made the skeleton

--- cky.py
+++ cky.py
@@ -8,4 +8,36 @@ class CKYParser(object):
 	def __init__(self, grammar):
 		self.grammar = grammar;
 
-		
+	# TODO
+	def checkMembership(self, tokens):
+		pass
+
+
+	# TODO
+	def backtrack(self, tokens):
+		pass
+
+def makeParseTree (???)
+# TODO
+
+def clean(a):
+	a = a.lower().strip().split()
+	# split words like ["cat."] -> ["cat", "."]
+	a[len(a)-1] = a[len(a)-1].replace(".", "")
+	a.append(".")
+	return a
+
+
+if __name__ == "__main__":
+
+	with open('probcfg.pcfg', 'r') as gramfile:
+		grammar = PCFG(gramfile)
+		parser = CKYParser(grammar)
+
+		sentence = "Flights from miami to cleveland"
+		inputokens = clean(sentence)
+
+		if checkMembership(inputokens):
+			# TODO 
+			# parse tree with backpointers
+			# print table
